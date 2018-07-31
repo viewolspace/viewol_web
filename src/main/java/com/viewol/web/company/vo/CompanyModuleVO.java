3ede4f365ccde6859f3bffb1718c354435db0339
wangdong <wangdong@youguu.com>
2018-07-31 16:25:16 +0800

    修改

--- src/main/java/com/viewol/web/company/vo/CompanyModuleVO.java
+++ src/main/java/com/viewol/web/company/vo/CompanyModuleVO.java
@@ -1,11 +1,13 @@
 package com.viewol.web.company.vo;
 
 import com.viewol.web.common.Response;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 /**
  * Created by lenovo on 2018/7/18.
  */
+@ApiModel
 public class CompanyModuleVO extends Response {
 
     @ApiModelProperty(value = "展商详细")
