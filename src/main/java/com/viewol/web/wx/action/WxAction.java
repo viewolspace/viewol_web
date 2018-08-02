35bf3400f3125a57355365b0bb5a01268d39a71d
shileishmily <shileibrave@163.com>
2018-08-02 15:06:51 +0800

    修改接口

--- src/main/java/com/viewol/web/wx/action/WxAction.java
+++ src/main/java/com/viewol/web/wx/action/WxAction.java
@@ -1,15 +1,22 @@
 package com.viewol.web.wx.action;
 
-import com.alibaba.fastjson.JSON;
-import com.viewol.pojo.WxUserInfo;
+import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
+import cn.binarywang.wx.miniapp.bean.WxMaUserInfo;
+import com.viewol.pojo.BUser;
+import com.viewol.pojo.FUser;
+import com.viewol.pojo.FUserBind;
+import com.viewol.service.IBUserService;
+import com.viewol.service.IFUserService;
 import com.viewol.service.IWxService;
 import com.viewol.web.common.Response;
 import com.viewol.web.ucard.vo.UserCardResponse;
+import com.viewol.web.wx.vo.BuserLoginResponse;
 import com.viewol.web.wx.vo.FollowResponse;
-import com.viewol.web.wx.vo.TokenResponse;
-import com.viewol.web.wx.vo.WxUserResponse;
-import com.youguu.core.util.ClassCast;
+import com.viewol.web.wx.vo.LoginResponse;
+import com.youguu.core.util.json.YouguuJsonHelper;
 import io.swagger.annotations.*;
+import me.chanjar.weixin.mp.bean.result.WxMpOAuth2AccessToken;
+import me.chanjar.weixin.mp.bean.result.WxMpUser;
 import org.springframework.stereotype.Controller;
 
 import javax.annotation.Resource;
@@ -30,37 +37,86 @@ public class WxAction {
 
     @Resource
     private IWxService wxService;
+    @Resource
+    private IFUserService fUserService;
+    @Resource
+    private IBUserService bUserService;
 
     /**
-     * 网页授权，获取access_token(非基础access_token)
-     * 第一步：用户同意授权，获取code
-     * 第二步：通过code换取网页授权access_token
-     * 第四步：拉取用户信息(需scope为 snsapi_userinfo)
-     * @param jscode
+     * 小程序自动登录
+     * @param code 临时授权code
      * @return
      */
     @GET
-    @Path(value = "/access_token")
+    @Path(value = "/maLogin")
     @Produces("text/html;charset=UTF-8")
-    @ApiOperation(value = "网页授权获取AccessToken", notes = "<p>第一步：用户同意授权，获取code</p><p>第二步：通过code换取网页授权access_token</p>", author = "更新于 2018-07-20")
+    @ApiOperation(value = "小程序自动登录", notes = "", author = "更新于 2018-08-02")
     @ApiResponses(value = {
-            @ApiResponse(code = "0000", message = "授权成功", response = TokenResponse.class),
-            @ApiResponse(code = "0002", message = "授权失败", response = Response.class),
+            @ApiResponse(code = "0000", message = "请求成功", response = UserCardResponse.class),
             @ApiResponse(code = "0001", message = "系统异常", response = Response.class)
     })
-    public String access_token(@ApiParam(value = "授权code", required = true) @QueryParam("jscode") String jscode) {
-        TokenResponse rs = new TokenResponse();
+    public String maLogin(@ApiParam(value = "授权code", required = true) @QueryParam("code") String code,
+                          @ApiParam(value = "消息密文,不需要获取微信用户信息可以不传") @QueryParam("encryptedData") String encryptedData,
+                          @ApiParam(value = "加密算法的初始向量,不需要获取微信用户信息可以不传") @QueryParam("ivStr") String ivStr) {
 
-        try{
-            String json = wxService.getAccessToken(jscode);
-            if(json != null && !"".equals(json)){
-                TokenResponse.Token token = JSON.parseObject(json, TokenResponse.Token.class);
+        LoginResponse rs = new LoginResponse();
+
+        try {
+            WxMaJscode2SessionResult wxMaJscode2Session = wxService.getSessionInfo(code);
+            if(wxMaJscode2Session == null){
+                return YouguuJsonHelper.returnJSON("0002", "授权失败");
+            }
+
+            String sessionKey = wxMaJscode2Session.getSessionKey();
+            String openid = wxMaJscode2Session.getOpenid();
+            String unionid = wxMaJscode2Session.getUnionid();
+
+            //已注册，返回用户信息
+            FUser fuser = fUserService.getUserByUuid(unionid);
+            if(fuser != null){
+                LoginResponse.UserInfo userInfo = rs.new UserInfo();
+                userInfo.setUserId(fuser.getUserId());
+                userInfo.setUserName(fuser.getUserName());
+                userInfo.setPhone(fuser.getPhone());
+                userInfo.setCompany(fuser.getCompany());
+                userInfo.setPosition(fuser.getPosition());
+                userInfo.setEmail(fuser.getEmail());
+                userInfo.setAge(fuser.getAge());
+                userInfo.setHeadImgUrl(fuser.getHeadImgUrl());
+                userInfo.setCompanyId(fuser.getCompanyId());
+                userInfo.setSessionId("");
+
                 rs.setStatus("0000");
                 rs.setMessage("授权成功");
-                rs.setResult(token);
+                rs.setResult(userInfo);
+                return rs.toJSONString();
+            }
+
+            //注册新用户
+            WxMaUserInfo wxMaUserInfo = wxService.getUserInfo(sessionKey, encryptedData, ivStr);
+            fuser = new FUser();
+            if(wxMaUserInfo != null){
+                fuser.setHeadImgUrl(wxMaUserInfo.getAvatarUrl());
+            }
+            fuser.setUuid(unionid);
+
+            int result = fUserService.addFUser(fuser, openid, unionid, FUserBind.TYPE_PROGRAM);
+
+            if(result > 0){
+                LoginResponse.UserInfo userInfo = rs.new UserInfo();
+                userInfo.setUserId(result);
+                userInfo.setHeadImgUrl(fuser.getHeadImgUrl());
+
+                //TODO 生成小观时讯的session
+                String session = "20180802112845";
+                userInfo.setSessionId(session);
+
+                rs.setStatus("0000");
+                rs.setMessage("授权成功");
+                rs.setResult(userInfo);
             } else {
-                rs.setStatus("0002");
-                rs.setMessage("授权失败");
+                rs.setStatus("0004");
+                rs.setMessage("注册失败");
             }
         } catch (Exception e){
             rs.setStatus("0001");
@@ -71,78 +127,80 @@ public class WxAction {
     }
 
     /**
-     * 网页授权，查询用户基本信息(该接口不会返回用户是否关注公众号字段)
-     * 第一步：用户同意授权，获取code
-     * 第二步：通过code换取网页授权access_token
-     * 第四步：拉取用户信息(需scope为 snsapi_userinfo)
-     * @param userId 用户ID
+     * H5自动授权登录
+     * @param jscode 临时授权code
      * @return
      */
     @GET
-    @Path(value = "/userinfo")
+    @Path(value = "/mpLogin")
     @Produces("text/html;charset=UTF-8")
-    @ApiOperation(value = "查询用户信息（已登录）", notes = "该接口不会返回用户是否关注公众号字段", author = "更新于 2018-07-23")
+    @ApiOperation(value = "H5自动授权登录", notes = "H5自动授权登录", author = "更新于 2018-08-02")
     @ApiResponses(value = {
-            @ApiResponse(code = "0000", message = "查询成功", response = WxUserResponse.class),
-            @ApiResponse(code = "0002", message = "查询失败", response = Response.class),
+            @ApiResponse(code = "0000", message = "授权成功", response = LoginResponse.class),
+            @ApiResponse(code = "0002", message = "授权失败", response = Response.class),
+            @ApiResponse(code = "0003", message = "获取第三方数据失败", response = Response.class),
             @ApiResponse(code = "0001", message = "系统异常", response = Response.class)
     })
-    public String userinfo(@ApiParam(value = "用户ID", required = true) @QueryParam("userId") int userId) {
-        WxUserResponse rs = new WxUserResponse();
+    public String mpLogin(@ApiParam(value = "临时授权code", required = true) @QueryParam("jscode") String jscode) {
+        LoginResponse rs = new LoginResponse();
 
         try{
-            WxUserInfo wxUserInfo = wxService.getUserInfo(userId);
-            if(wxUserInfo != null){
-                WxUserResponse.WxUser wxUser = ClassCast.cast(wxUserInfo, WxUserResponse.WxUser.class);
-
-                rs.setStatus("0000");
-                rs.setMessage("查询成功");
-                rs.setResult(wxUser);
-            } else {
-                rs.setStatus("0002");
-                rs.setMessage("查询失败");
+            WxMpOAuth2AccessToken wxMpOAuth2AccessToken = wxService.getAccessToken(jscode);
+            if(wxMpOAuth2AccessToken == null){
+                return YouguuJsonHelper.returnJSON("0002", "授权失败");
             }
-        } catch (Exception e){
-            rs.setStatus("0001");
-            rs.setMessage("系统异常");
-        }
 
-        return rs.toJSONString();
-    }
+            String accessToken = wxMpOAuth2AccessToken.getAccessToken();
+            String openid = wxMpOAuth2AccessToken.getOpenId();
+            String unionId = wxMpOAuth2AccessToken.getUnionId();
 
-    /**
-     * 网页授权，查询用户基本信息，未注册情况(该接口不会返回用户是否关注公众号字段)
-     * 第一步：用户同意授权，获取code
-     * 第二步：通过code换取网页授权access_token
-     * 第四步：拉取用户信息(需scope为 snsapi_userinfo)
-     * @param access_token (非基础access_token)
-    *  @param openid
-     * @return
-     */
-    @GET
-    @Path(value = "/user/info")
-    @Produces("text/html;charset=UTF-8")
-    @ApiOperation(value = "网页授权获取用户信息", notes = "适用未注册时调用，该接口不会返回用户是否关注公众号字段", author = "更新于 2018-07-23")
-    @ApiResponses(value = {
-            @ApiResponse(code = "0000", message = "查询成功", response = WxUserResponse.class),
-            @ApiResponse(code = "0002", message = "查询失败", response = Response.class),
-            @ApiResponse(code = "0001", message = "系统异常", response = Response.class)
-    })
-    public String userinfo(@ApiParam(value = "access_token", required = true) @QueryParam("access_token") String access_token,
-                           @ApiParam(value = "openid", required = true) @QueryParam("openid") String openid) {
-        WxUserResponse rs = new WxUserResponse();
-
-        try{
-            WxUserInfo wxUserInfo = wxService.getUserInfo(access_token, openid);
-            if(wxUserInfo != null){
-                WxUserResponse.WxUser wxUser = ClassCast.cast(wxUserInfo, WxUserResponse.WxUser.class);
+            //已注册，返回用户信息
+            FUser fuser = fUserService.getUserByUuid(unionId);
+            if(fuser != null){
+                LoginResponse.UserInfo userInfo = rs.new UserInfo();
+                userInfo.setUserId(fuser.getUserId());
+                userInfo.setUserName(fuser.getUserName());
+                userInfo.setPhone(fuser.getPhone());
+                userInfo.setCompany(fuser.getCompany());
+                userInfo.setPosition(fuser.getPosition());
+                userInfo.setEmail(fuser.getEmail());
+                userInfo.setAge(fuser.getAge());
+                userInfo.setHeadImgUrl(fuser.getHeadImgUrl());
+                userInfo.setCompanyId(fuser.getCompanyId());
+                userInfo.setSessionId("");
 
                 rs.setStatus("0000");
-                rs.setMessage("查询成功");
-                rs.setResult(wxUser);
+                rs.setMessage("授权成功");
+                rs.setResult(userInfo);
+                return rs.toJSONString();
+            }
+
+            WxMpUser wxMpUser = wxService.getUserInfo(accessToken, openid);
+            if(wxMpUser == null){
+                return YouguuJsonHelper.returnJSON("0003", "获取第三方数据失败");
+            }
+
+            fuser = new FUser();
+            fuser.setHeadImgUrl(wxMpUser.getHeadImgUrl());
+            fuser.setUuid(unionId);
+
+            int result = fUserService.addFUser(openid, unionId, FUserBind.TYPE_WEIXIN);
+
+            if(result > 0){
+                LoginResponse.UserInfo userInfo = rs.new UserInfo();
+                userInfo.setUserId(result);
+                userInfo.setHeadImgUrl(fuser.getHeadImgUrl());
+
+                //TODO 生成小观时讯的session
+                String session = "20180802112845";
+                userInfo.setSessionId(session);
+
+                rs.setStatus("0000");
+                rs.setMessage("授权成功");
+                rs.setResult(userInfo);
             } else {
-                rs.setStatus("0002");
-                rs.setMessage("查询失败");
+                rs.setStatus("0004");
+                rs.setMessage("注册失败");
             }
         } catch (Exception e){
             rs.setStatus("0001");
@@ -181,16 +239,93 @@ public class WxAction {
         return rs.toJSONString();
     }
 
+
+    /**
+     * 展商业务员扫码登录接口
+     * @param jscode
+     * @return
+     */
     @GET
-    @Path(value = "/getSessionInfo")
+    @Path(value = "/bUserLogin")
     @Produces("text/html;charset=UTF-8")
-    @ApiOperation(value = "小程序授权获取openid", notes = "", author = "更新于 2018-07-20")
+    @ApiOperation(value = "展商业务员扫码登录接口", notes = "展商业务员通过扫描展商管理后台的二维码，获取H5访问权限。", author = "更新于 2018-08-02")
     @ApiResponses(value = {
-            @ApiResponse(code = "0000", message = "请求成功", response = UserCardResponse.class),
+            @ApiResponse(code = "0000", message = "授权成功", response = LoginResponse.class),
+            @ApiResponse(code = "0002", message = "授权失败", response = Response.class),
+            @ApiResponse(code = "0003", message = "获取第三方数据失败", response = Response.class),
             @ApiResponse(code = "0001", message = "系统异常", response = Response.class)
     })
-    public String getSessionInfo(@ApiParam(value = "授权code", required = true) @QueryParam("jscode") String jscode) {
-        String result = wxService.getSessionInfo(jscode);
-        return result;
+    public String bUserLogin(@ApiParam(value = "临时授权code", required = true) @QueryParam("jscode") String jscode,
+                             @ApiParam(value = "展商ID", required = true) @QueryParam("companyId") int companyId) {
+        BuserLoginResponse rs = new BuserLoginResponse();
+
+        try{
+            WxMpOAuth2AccessToken wxMpOAuth2AccessToken = wxService.getAccessToken(jscode);
+            if(wxMpOAuth2AccessToken == null){
+                return YouguuJsonHelper.returnJSON("0002", "授权失败");
+            }
+
+            String accessToken = wxMpOAuth2AccessToken.getAccessToken();
+            String openid = wxMpOAuth2AccessToken.getOpenId();
+            String unionId = wxMpOAuth2AccessToken.getUnionId();
+
+            //业务员已注册，返回业务员信息
+            BUser bUser = bUserService.getBUser(openid);
+            if(bUser!=null){
+                BuserLoginResponse.UserInfo userInfo = rs.new UserInfo();
+                userInfo.setUserId(bUser.getUserId());
+                userInfo.setUserName(bUser.getUserName());
+                userInfo.setPhone(bUser.getPhone());
+                userInfo.setPosition(bUser.getPosition());
+                userInfo.setHeadImgUrl(bUser.getHeadImgUrl());
+                userInfo.setCompanyId(bUser.getCompanyId());
+                userInfo.setSessionId("");
+                userInfo.setStatus(bUser.getStatus());
+
+
+                rs.setStatus("0000");
+                rs.setMessage("授权成功");
+                rs.setResult(userInfo);
+                return rs.toJSONString();
+            }
+
+            //注册
+            WxMpUser wxMpUser = wxService.getUserInfo(accessToken, openid);
+            if(wxMpUser == null){
+                return YouguuJsonHelper.returnJSON("0003", "获取第三方数据失败");
+            }
+
+            bUser = new BUser();
+            bUser.setCompanyId(companyId);
+            bUser.setOpenId(openid);
+            bUser.setUuid(unionId);
+            bUser.setHeadImgUrl(wxMpUser.getHeadImgUrl());
+            bUser.setStatus(BUser.STATUS_TRIAL);//0-待审
+
+            int result = bUserService.addBUser(bUser);
+
+            if(result > 0){
+                BuserLoginResponse.UserInfo userInfo = rs.new UserInfo();
+                userInfo.setUserId(result);
+                userInfo.setHeadImgUrl(bUser.getHeadImgUrl());
+                userInfo.setStatus(BUser.STATUS_TRIAL);
+
+                //TODO 生成小观时讯的session
+                String session = "20180802112845";
+                userInfo.setSessionId(session);
+
+                rs.setStatus("0000");
+                rs.setMessage("授权成功");
+                rs.setResult(userInfo);
+            } else {
+                rs.setStatus("0004");
+                rs.setMessage("注册失败");
+            }
+        } catch (Exception e){
+            rs.setStatus("0001");
+            rs.setMessage("系统异常");
+        }
+
+        return rs.toJSONString();
     }
 }
