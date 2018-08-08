5588ef2d32c7ffeca0bc3ce3c84d516b779766b0
wangdong <wangdong@youguu.com>
2018-08-08 15:42:52 +0800

    自该

--- src/main/java/com/viewol/web/ucard/action/ScanCardAction.java
+++ src/main/java/com/viewol/web/ucard/action/ScanCardAction.java
@@ -1,11 +1,15 @@
 package com.viewol.web.ucard.action;
 
+import com.alibaba.fastjson.JSONObject;
+import com.viewol.cardscan.IScanCard;
+import com.viewol.cardscan.ScanTestXY;
 import io.swagger.annotations.*;
 import net.coobird.thumbnailator.Thumbnails;
 import org.apache.commons.io.IOUtils;
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import org.springframework.stereotype.Controller;
+import sun.misc.BASE64Encoder;
 
 import javax.imageio.ImageIO;
 import javax.ws.rs.Consumes;
@@ -18,7 +22,9 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by lenovo on 2018/8/1.
@@ -47,6 +53,7 @@ public class ScanCardAction {
         InputStream is = null;
         ByteArrayOutputStream os = null;
         ByteArrayInputStream bis = null;
+        JSONObject json = null;
         try {
 
             List<InputPart> list=input.getFormDataMap().get("img");
@@ -83,7 +90,17 @@ public class ScanCardAction {
                     System.out.println(os.toByteArray().length);
                 }
 
+                BASE64Encoder encoder = new BASE64Encoder();
+                String base64 = encoder.encode(os.toByteArray());
+                Map<String,String> map = new HashMap<>();
+                map.put("base64",base64);
 
+//                IScanCard sc = new ScanTestHW();
+//                System.out.println("hw:" + sc.scan(map));
+
+                IScanCard sc1 = new ScanTestXY();
+                json = sc1.scan(map);
+                System.out.println("xy:" + sc1.scan(map));
 
 
 //                //判断图片大小
@@ -105,6 +122,8 @@ public class ScanCardAction {
             }
 
         } catch (Exception e) {
+            json = new JSONObject();
+            json.put("status","0001");
             e.printStackTrace();
         }finally {
             if(is!=null){ try {is.close();} catch (IOException e) {e.printStackTrace();}}
@@ -112,7 +131,7 @@ public class ScanCardAction {
             if(bis!=null){ try {bis.close();} catch (IOException e) {e.printStackTrace();}}
         }
 
-        return "{\"status\":\"ok\"}";
+        return json.toJSONString();
     }
 
 
