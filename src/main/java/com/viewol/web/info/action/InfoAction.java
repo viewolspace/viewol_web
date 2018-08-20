36b94b1353216d652d725104be9b88ed5dd1a478
shileishmily <shileibrave@163.com>
2018-08-20 14:45:13 +0800

    新闻接口

---
+++ src/main/java/com/viewol/web/info/action/InfoAction.java
@@ -0,0 +1,72 @@
+package com.viewol.web.info.action;
+
+import com.viewol.pojo.Info;
+import com.viewol.service.IInfoService;
+import com.viewol.web.info.vo.InfoResponse;
+import io.swagger.annotations.*;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.Resource;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import java.util.ArrayList;
+import java.util.List;
+
+@SwaggerDefinition(
+        tags = {
+                @Tag(name="v1.0",description="资讯")
+        }
+)
+@Api(value = "InfoAction")
+@Path(value = "info")
+@Controller("infoAction")
+public class InfoAction {
+
+    @Resource
+    private IInfoService infoService;
+
+    @GET
+    @Path(value = "/list")
+    @Produces("text/html;charset=UTF-8")
+    @ApiOperation(value = "查询资讯列表", notes = "", author = "更新于 2018-08-20")
+    @ApiResponses(value = {
+            @ApiResponse(code = "0000", message = "请求成功" ,response = InfoResponse.class),
+
+    })
+    public String infoList(@ApiParam(value = "最后一条记录ID", defaultValue = "0", required = true) @QueryParam("lastSeq") int lastSeq,
+                           @ApiParam(value = "每页多少条记录", defaultValue = "10", required = true) @QueryParam("pageSize") int pageSize) {
+        InfoResponse rs = new InfoResponse();
+        try{
+            List<Info> list =  infoService.listInfo(lastSeq, pageSize);
+            if(list!=null && list.size()>0){
+                rs.setStatus("0000");
+                rs.setMessage("暂无数据");
+                List<InfoResponse.InfoVO> result = new ArrayList<>();
+                for(Info info : list){
+                    InfoResponse.InfoVO infoVO = rs.new InfoVO();
+                    infoVO.setId(info.getId());
+                    infoVO.setTitle(info.getTitle());
+                    infoVO.setSummary(info.getSummary());
+                    infoVO.setPubTime(info.getPubTime());
+                    infoVO.setPicUrl(info.getPicUrl());
+                    infoVO.setContentUrl(info.getContentUrl());
+
+                    result.add(infoVO);
+                }
+                rs.setResult(result);
+            } else {
+                rs.setStatus("0000");
+                rs.setMessage("暂无数据");
+            }
+        } catch (Exception e){
+            rs.setStatus("0001");
+            rs.setMessage("系统异常");
+        }
+
+
+        return rs.toJSONString();
+
+    }
+}
---
+++ src/main/java/com/viewol/web/info/vo/InfoResponse.java
@@ -0,0 +1,103 @@
+package com.viewol.web.info.vo;
+
+import com.alibaba.fastjson.JSON;
+import com.viewol.web.common.Response;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.Date;
+import java.util.List;
+
+@ApiModel
+public class InfoResponse extends Response {
+
+    private List<InfoVO> result;
+
+    public InfoResponse() {}
+
+    public InfoResponse(String status, String message) {
+        super(status, message);
+    }
+
+
+    public List<InfoVO> getResult() {
+        return result;
+    }
+
+    public void setResult(List<InfoVO> result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toJSONString() {
+        return JSON.toJSONString(this);
+    }
+
+    public class InfoVO{
+        @ApiModelProperty(value = "新闻ID")
+        private int id;
+
+        @ApiModelProperty(value = "标题")
+        private String title;
+
+        @ApiModelProperty(value = "摘要")
+        private String summary;
+
+        @ApiModelProperty(value = "发布日期")
+        private Date pubTime;
+
+        @ApiModelProperty(value = "图片地址")
+        private String picUrl;
+
+        @ApiModelProperty(value = "正文地址")
+        private String contentUrl;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getSummary() {
+            return summary;
+        }
+
+        public void setSummary(String summary) {
+            this.summary = summary;
+        }
+
+        public Date getPubTime() {
+            return pubTime;
+        }
+
+        public void setPubTime(Date pubTime) {
+            this.pubTime = pubTime;
+        }
+
+        public String getPicUrl() {
+            return picUrl;
+        }
+
+        public void setPicUrl(String picUrl) {
+            this.picUrl = picUrl;
+        }
+
+        public String getContentUrl() {
+            return contentUrl;
+        }
+
+        public void setContentUrl(String contentUrl) {
+            this.contentUrl = contentUrl;
+        }
+    }
+}
