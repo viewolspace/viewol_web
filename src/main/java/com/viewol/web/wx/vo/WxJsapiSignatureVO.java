f68df1882592da71deaefdfc1e6068dc7ebb097d
wangdong <wangdong@youguu.com>
2018-08-23 11:50:20 +0800

    修改

--- src/main/java/com/viewol/web/wx/vo/WxJsapiSignatureVO.java
+++ src/main/java/com/viewol/web/wx/vo/WxJsapiSignatureVO.java
@@ -1,17 +1,26 @@
 package com.viewol.web.wx.vo;
 
 import com.viewol.web.common.Response;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 /**
  * Created by lenovo on 2018/8/22.
  */
+@ApiModel
 public class WxJsapiSignatureVO extends Response {
 
 
     @ApiModelProperty(value = "授权信息")
     private WxJsapiSignatureMo result;
 
+    public WxJsapiSignatureMo getResult() {
+        return result;
+    }
+
+    public void setResult(WxJsapiSignatureMo result) {
+        this.result = result;
+    }
 
     class WxJsapiSignatureMo{
         @ApiModelProperty(value = "appId")
